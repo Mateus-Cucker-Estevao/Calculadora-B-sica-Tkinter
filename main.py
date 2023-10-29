0776caf880215516345703edffae11a9f2680123
Mateus Ramos <mateusestevao44@gmail.com>
2023-10-29 15:41:20 -0300

    Botões, e label do visor para os numeros.

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ frame_tela.grid(row=0, column=0)
 frame_corpo = Frame(janela, width=235, height=268)
 frame_corpo.grid(row=1, column=0)
 
+#Criando LABEL (Onde ira aparecer os numeros)
+app_label = Label(frame_tela, text="123456789", width=16, height=2, padx=7, relief=FLAT, anchor="e", justify=RIGHT, font=('Ivy 18'), bg=cor3, fg=cor2)
+app_label.place(x=0, y=0)
+
 #criando botoes
 b_1 = Button(frame_corpo, text="C", width=11, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
@@ -58,12 +62,12 @@ b_14.place(x=118, y=156)
 b_15 = Button(frame_corpo, text="+", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_15.place(x=177, y=156)
 
-b_1 = Button(frame_corpo, text="0", width=11, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-b_1.place(x=0, y=208)
-b_2 = Button(frame_corpo, text=".", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-b_2.place(x=118, y=208)
-b_3 = Button(frame_corpo, text="=", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
-b_3.place(x=177, y=208)
+b_16 = Button(frame_corpo, text="0", width=11, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_16.place(x=0, y=208)
+b_17 = Button(frame_corpo, text=".", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_17.place(x=118, y=208)
+b_18 = Button(frame_corpo, text="=", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_18.place(x=177, y=208)
 
 
 janela.mainloop() 
