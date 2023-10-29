3af146cbb80b42077179b08733480c9132be0ad0
Mateus Ramos <mateusestevao44@gmail.com>
2023-10-29 10:00:06 -0300

    Criandos botões - Altura janela 310

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ cor5 = "#FFAB40"  # laranja
 #Criar uma janela
 janela = Tk()
 janela.title("Calculadora")
-janela.geometry("235x318")
+janela.geometry("235x310")
 janela.config(bg=cor1)
 
 #Divisao do visor
@@ -39,6 +39,31 @@ b_6 = Button(frame_corpo, text="9", width=5, height=2, bg=cor4, fg=cor1, font=('
 b_6.place(x=118, y=52)
 b_7 = Button(frame_corpo, text="*", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_7.place(x=177, y=52)
+
+b_8 = Button(frame_corpo, text="4", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_8.place(x=0, y=104)
+b_9 = Button(frame_corpo, text="5", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_9.place(x=60, y=104)
+b_10 = Button(frame_corpo, text="6", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_10.place(x=118, y=104)
+b_11 = Button(frame_corpo, text="-", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_11.place(x=177, y=104)
+
+b_12 = Button(frame_corpo, text="1", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_12.place(x=0, y=156)
+b_13 = Button(frame_corpo, text="2", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_13.place(x=60, y=156)
+b_14 = Button(frame_corpo, text="3", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_14.place(x=118, y=156)
+b_15 = Button(frame_corpo, text="+", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_15.place(x=177, y=156)
+
+b_1 = Button(frame_corpo, text="0", width=11, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_1.place(x=0, y=208)
+b_2 = Button(frame_corpo, text=".", width=5, height=2, bg=cor4, fg=cor1, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_2.place(x=118, y=208)
+b_3 = Button(frame_corpo, text="=", width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_3.place(x=177, y=208)
 
 
 janela.mainloop() 
